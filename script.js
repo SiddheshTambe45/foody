9e959716d40a9dea391696b14a54a8301db52445
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-03-09 12:04:09 +0530

    aos animations for header mainly

--- script.js
+++ script.js
@@ -12,5 +12,27 @@ $("document").ready(function(){
 	
 	$('.header').css("padding-top",navHeight+10);
 	*/
+	/*
+	function adjustAOSBasedOnScreenWidth() {
+		if ($(window).width() > 768) {
+			$('#delicious').attr('data-aos', 'fade-right');
+			$('#food').attr('data-aos', 'zoom-in-up');
+			$('#dishes').attr('data-aos', 'fade-left');
+		} else {
+			$('#delicious').attr('data-aos','flip-down');
+			$('#food').attr('data-aos','zoom-in');
+			$('#dishes').attr('data-aos','flip-up');
+		}
+	}	
 	
+	$(window).resize(function() {
+		adjustAOSBasedOnScreenWidth();
+	});
+	*/
+
 });
+/*
+$(document).ready(function() {
+    adjustAOSBasedOnScreenWidth();
+});
+*/
